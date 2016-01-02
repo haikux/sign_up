1eced3e768acdfffd958b886f2984c99bd161235
harish-halo <your.harishrsk11@gmail.com>
2016-01-02 22:33:05 +0530

    make a basic User model(INcludind  a secure password)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_t(page_t)
+        base_t="ROR"
+        if page_t.empty?
+            base_t
+        else
+            page_t +"|"+base_t
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-      assert_select "title" , "home|RoR"
+      assert_select "title" , "ROR"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-      assert_select "title" , "help|RoR"
+      assert_select "title" , "help|ROR"
   end
   test "should get about" do
     get :about
     assert_response :success
-      assert_select "title" , "about|RoR"
+      assert_select "title" , "about|ROR"
   end
   
 end
