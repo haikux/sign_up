0b8132f886096b36383e330a02739d08572d3020
harish-halo <your.harishrsk11@gmail.com>
2016-01-10 11:21:14 +0530

    Finish sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-    def full_t(page_t)
-        base_t="ROR"
-        if page_t.empty?
-            base_t
-        else
-            page_t +"|"+base_t
-        end
-    end
 end
